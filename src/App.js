f4046567ddef6b5e6324e356e5bc7cedd3e4da10
AkashSDas <aakashdas368@gmail.com>
2020-07-18 09:20:12 +0530

    Update App.js to test LandingPage component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 
-import Header from "./components/Header";
-import QuizGame from "./components/QuizGame";
+// import Header from "./components/Header";
+// import QuizGame from "./components/QuizGame";
+
+import LandingPage from "./components/LandingPage/LandingPage";
 
 import "./style/App.css";
+import "./style/base.css";
 
 export default class App extends React.Component {
   render() {
     return (
       <div id={"App"}>
-        <Header />
-        <QuizGame />
+        <LandingPage></LandingPage>
       </div>
     );
   }
